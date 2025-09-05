4be884568ea9670598c45ac0d340911a4ae742fa
Michelle Silva <92028812+mihsilva@users.noreply.github.com>
2025-09-05 17:20:54 -0300

    Remover estrutura duplicada

--- QA Estudo Automação/QA Estudo Automação/test_button.cy.js
+++
@@ -1,7 +0,0 @@
-describe('Botão de exemplo', () => {
-  it('Deve clicar no botão', () => {
-    cy.visit('https://exemplo.com')
-    cy.get('button#meuBotao').click()
-    cy.contains('Botão clicado com sucesso')
-  })
-})
--- QA Estudo Automação/QA Estudo Automação/test_open_google.robot
+++
@@ -1,8 +0,0 @@
-*** Settings ***
-Library    SeleniumLibrary
-
-*** Test Cases ***
-Open Google
-    Open Browser    https://www.google.com    chrome
-    Title Should Be    Google
-    [Teardown]    Close Browser
